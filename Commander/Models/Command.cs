01d45907bb24dd1613f8f087b5c17333f823f994
Austin Wright <austin@silentbreaksecurity.com>
2020-12-07 14:23:16 -0700

    Created the Model and Controller for the api endpoints.

--- Commander/Models/Command.cs
+++ Commander/Models/Command.cs
@@ -2,7 +2,7 @@ namespace Commander.Models
 {
   public class Command
   {
-    public int ID { get; set; }
+    public int Id { get; set; }
     public string HowTo { get; set; }
     public string Line { get; set; }
     public string Platform { get; set; }
---
+++ README.md
@@ -0,0 +1,1 @@
+Repo for learning ASP.NET Core
